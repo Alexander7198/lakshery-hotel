ab6c9547f46a3e786de715bd8d6f951e1cf735eb
Alexander Dorofeev <alexandedorofeev@yandex.ru>
2024-02-12 16:49:29 +0300

    seven

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,28 @@ import EditRoom from './components/room/EditRoom'
 import Home from './components/home/Home'
 import ExistingRooms from './components/room/ExistingRooms'
 import AddRoom from './components/room/AddRoom'
-
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import RoomListing from './components/room/RoomListing'
+import Admin from './components/admin/Admin'
 
 
 function App() {
   return <>
     <main>
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/edit-room/:roomId" element={<EditRoom />} />
           <Route path="/existing-rooms" element={<ExistingRooms />} />
           <Route path="/add-room" element={<AddRoom />} />
+          <Route path="/browse-all-rooms" element={<RoomListing />} />
+          <Route path="/admin" element={<Admin />} />
+
         </Routes>
       </Router>
+      <Footer />
     </main>
   </>
 }
